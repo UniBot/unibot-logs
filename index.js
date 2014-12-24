d2b9c8fce1fb916ac8750dedcdf3649b24fe861b
Dean Sofer <dean@Deans-MacBook-Pro.local>
2014-12-23 17:00:51 -0800

    Refactored structure for new plugin init code

--- index.js
+++ index.js
@@ -1,53 +1,65 @@
-var mongoose  = require('mongoose');
+/**
+ * Commands Plugin for Unibot
+ * @param  {Object} options [description]
+ *   db: {mongoose} the mongodb connection
+ *   bot: {irc} the irc bot
+ *   web: {connect} a connect + connect-rest webserver
+ *   config: {object}
+ * @return {Function}         init function to access shared resources
+ */
+module.exports = function init(options){
 
-var Logs = new mongoose.Schema({
-  channel : {
-    type  : String, // channel._id
-    index : {
-      unique   : true,
-      dropDups : false
-    }
-  },
-  logs : {
-    type: [mongoose.Schema.Types.Mixed],
-    default: []
-  }
-});
+  var mongoose = options.db;
+  var bot = options.bot;
+  var webserver = options.web;
+  var config = options.config;
 
-var model = mongoose.model('Logs', Logs);
-
-var plugin = function(channel, config){
-  var logs = [];
-
-  model.findOne({ channel: channel.id }, function(err, _logs_){
-    if (err || !_logs_) {
-      logs = new model({
-        channel: channel.id
-      });
-      logs.save();
-    } else {
-      logs = _logs_;
+  var Logs = new mongoose.Schema({
+    channel : {
+      type  : String, // channel._id
+      index : {
+        unique   : true,
+        dropDups : false
+      }
+    },
+    logs : {
+      type: [mongoose.Schema.Types.Mixed],
+      default: []
     }
   });
 
-  return {
-    "(.+)": function(from, matches) {
-      logs.logs.unshift({ from: from, message: matches[0], timestamp: Date.now() });
-      logs.markModified('logs');
-      logs.save();
-    }
-  };
-};
+  var model = mongoose.model('Logs', Logs);
 
-plugin.load = function(app) {
-  app.get('/logs', function(req, res, next){
+  webserver.get('/logs', function(req, res, next){
     res.sendFile('index.html');
   });
-  app.get('/logs/:channel', function(req, res, next) {
+
+  webserver.get('/logs/:channel', function(req, res, next) {
     model.findOne({ channel: req.params.channel }, function(err, logs){
       res.send(err || logs);
     });
   });
-};
 
-module.exports = plugin;
+  return function plugin(channel){
+    var logs = [];
+
+    model.findOne({ channel: channel.id }, function(err, _logs_){
+      if (err || !_logs_) {
+        logs = new model({
+          channel: channel.id
+        });
+        logs.save();
+      } else {
+        logs = _logs_;
+      }
+    });
+
+    return {
+      "(.+)": function(from, matches) {
+        logs.logs.unshift({ from: from, message: matches[0], timestamp: Date.now() });
+        logs.markModified('logs');
+        logs.save();
+      }
+    };
+  };
+};
